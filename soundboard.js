38001359baba0e9b5f15711bcfe9a43d400e2dad
jjet1716 <nagyjos2@msu.edu>
2022-06-16 13:50:05 -0400

    fixed js

--- soundboard.js
+++ soundboard.js
@@ -1,12 +1,10 @@
-const soundboard = {
-    "ring" : "https://www.wavsource.com/snds_2020-10-01_3728627494378403/sfx/phone_ring2.wav",
-    "slacker" : "https://www.wavsource.com/snds_2020-10-01_3728627494378403/video_games/duke/back_2_work_y.wav",
-    "slam" : "https://www.wavsource.com/snds_2020-10-01_3728627494378403/sfx/phone_slam.wav"
+var playsound = function(e) {
+    console.log('playing sound');
+    let audioEle = e.target.querySelector('audio');
+    audioEle.play();
 }
 
-function playsound(sound) {
-    console.log('playing', sound);
-
-    audioObj = new Audio("https://www.wavsource.com/snds_2020-10-01_3728627494378403/sfx/phone_ring2.wav");
-    audioObj.play();
-}
+var buttons = document.querySelectorAll('#soundboard button');
+buttons.forEach( (btn) => {
+    btn.addEventListener('click', playsound);
+})
