ac7a36ab6c4f8be949365c9318c4f03f21902051
jjet1716 <nagyjos2@msu.edu>
2022-06-16 13:13:57 -0400

    added html and js

---
+++ soundboard.js
@@ -0,0 +1,10 @@
+const soundboard = {
+    "ring" : "https://www.wavsource.com/snds_2020-10-01_3728627494378403/sfx/phone_ring2.wav",
+    "slacker" : "https://www.wavsource.com/snds_2020-10-01_3728627494378403/video_games/duke/back_2_work_y.wav",
+    "slam" : "https://www.wavsource.com/snds_2020-10-01_3728627494378403/sfx/phone_slam.wav"
+}
+
+function playsound(sound) {
+    audioObj = new Audio(soundboard[sound]);
+    audioObj.play();
+}
